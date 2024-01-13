fb49248a67947926a6fed7f708e0535da35842c3
gulfamkhan2308 <gulfamkhan2308@gmail.com>
2024-01-13 00:07:20 -0800

    new updates

--- object oreandted programme/oop.mjs
+++ object oreandted programme/oop.mjs
@@ -1,3 +1,60 @@
+let person1 = {
+    name : 'nasir',
+    age :23,
+}
+
+function Person(name,age,city,course){
+    this.name = name
+    this.age = age
+    this.city = city
+    this.course =course
+}
+
+
+class Student{
+    #name;
+    #age;
+    #city;
+    #course;
+    #gender;
+    #institute;
+
+    constructor (name,age,city,gender,course){
+        this.#name = name
+        this.#age = age
+        this.#city = city
+        this.#course =course
+        this.#gender = gender
+        this.#institute = 'SMIT'
+
+    }
+
+    get name(){
+        return this.#name
+    }
+
+    set name(new_name){
+        if (new_name==='hamza'){
+            console.log(You can have not a name ${new_name})
+        }
+        else{
+            this.#name = new_name
+        }
+
+        
+    }
+
+
+}
+
+
+s1 = new Student('asad',23,'Lahore','Male','AI')
+s2 = new Student('rehan',23,'Lahore','Male','AI')
+s3 = new Student('sana',23,'Lahore','Male','AI')
+
+
+
+
 
 class Vehicle 
     {
